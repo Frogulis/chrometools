20364cfce737278f9af6e397cf12b3071c192620
Jamie Hoffmann <jamsarhoff@hotmail.com>
2018-03-25 01:00:53 +1100

    removed guff

--- nofacebook/deletefacebook.js
+++ nofacebook/deletefacebook.js
@@ -1,7 +1,3 @@
-/*chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab) {
-    console.log("qwe");
-});*/
-
 chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
     if (tab.url.match("https://www.facebook.com/?(\\?ref=tn_tnmn)?$") &&
         changeInfo.url === undefined) {
